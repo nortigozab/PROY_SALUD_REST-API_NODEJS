f52ac1246c8c4db6a5b2e008eeebc5d60d017447
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-25 02:20:02 -0500

    Model of doctor basic

---
+++ models/doctor.ts
@@ -0,0 +1,113 @@
+//file models/doctor.ts
+import { BasicDoctor, Doctor, DoctorWithDetails } from "../types/doctor";
+import { db } from "../db";
+import { OkPacket, RowDataPacket } from "mysql2";
+export const create = (doctor: Doctor, callback: Function) => {
+  const queryString =
+    "INSERT INTO Doctores (Nombre, Apellido, Especialidad, Consultorio, CorreoContacto) VALUES(?, ?, ?, ?, ?)";
+  db.query(
+    queryString,
+    [
+      doctor.nombre,
+      doctor.apellido,
+      doctor.especialidad.especialidadId,
+      doctor.consultorio.consultorioId,
+      doctor.correoContacto,
+    ],
+    (err, result) => {
+      if (err) {
+        callback(err);
+      }
+      const insertId = (<OkPacket>result).insertId;
+      callback(null, insertId);
+    }
+  );
+};
+export const findOne = (doctorId: number, callback: Function) => {
+  const queryString = `
+  SELECT D.IdDoctor,D.Nombre,D.Apellido,E.IdEspecialidad,E.NombreEspecialidad,C.IdConsultorio, C.Piso, C.NumeroConsultorio,D.CorreoContacto
+  FROM Doctores D JOIN Consultorios C ON D.Consultorio = C.IdConsultorio 
+  JOIN Especialidades E ON D.Especialidad = E.IdEspecialidad WHERE D.IdDoctor =?`;
+
+  db.query(queryString, doctorId, (err, result) => {
+    if (err) {
+      callback(err);
+    }
+    const row = (<RowDataPacket>result)[0];
+    const doctor: DoctorWithDetails = {
+      doctorId: row.IdDoctor,
+      especialidad: {
+        especialidadId: row.IdEspecialidad,
+        nombreEspecialidad: row.NombreEspecialidad,
+      },
+      nombre: row.Nombre,
+      apellido: row.Apellido,
+      consultorio: {
+        consultorioId: row.IdConsultorio,
+        divisionPiso: row.Piso,
+        numeroConsultorio: row.NumeroConsultorio,
+      },
+      correoContacto: row.CorreoContacto,
+    };
+    callback(null, doctor);
+  });
+};
+export const findAll = (callback: Function) => {
+  const queryString = `
+  SELECT D.IdDoctor,D.Nombre,D.Apellido,E.IdEspecialidad,E.NombreEspecialidad,C.IdConsultorio, C.Piso, C.NumeroConsultorio,D.CorreoContacto
+  FROM Doctores D JOIN Consultorios C ON D.Consultorio = C.IdConsultorio 
+  JOIN Especialidades E ON D.Especialidad = E.IdEspecialidad`;
+
+  db.query(queryString, (err, result) => {
+    if (err) {
+      callback(err);
+    }
+
+    const rows = <RowDataPacket[]>result;
+    const doctores: Doctor[] = [];
+
+    rows.forEach((row) => {
+      const doctor: DoctorWithDetails = {
+        doctorId: row.IdDoctor,
+        especialidad: {
+          especialidadId: row.IdEspecialidad,
+          nombreEspecialidad: row.NombreEspecialidad,
+        },
+        nombre: row.Nombre,
+        apellido: row.Apellido,
+        consultorio: {
+          consultorioId: row.IdConsultorio,
+          divisionPiso: row.Piso,
+          numeroConsultorio: row.NumeroConsultorio,
+        },
+        correoContacto: row.CorreoContacto,
+      };
+      doctores.push(doctor);
+    });
+    callback(null, doctores);
+  });
+};
+export const update = (doctor: Doctor, callback: Function) => {
+  const queryString =
+    "UPDATE Doctores SET Nombre = ?, Apellido = ?, Especialidad = ?, Consultorio = ?, CorreoContacto = ?WHERE IdDoctor = ?";
+
+  db.query(
+    queryString,
+    [
+      doctor.nombre,
+      doctor.apellido,
+      doctor.especialidad.especialidadId,
+      doctor.consultorio.consultorioId,
+      doctor.correoContacto,
+      doctor.doctorId,
+    ],
+    (err, result) => {
+      if (err) {
+        callback(err);
+      }
+
+      const numUpdate = (<OkPacket>result).affectedRows;
+      callback(null, numUpdate);
+    }
+  );
+};
