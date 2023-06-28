f99fd9ece0a159aa34d978b9e52cf673fcb99c2f
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-27 19:48:02 -0500

    feat: EJS como motor de plantilla, page 404

--- app.ts
+++ app.ts
@@ -2,14 +2,29 @@ import * as dotenv from "dotenv";
 import express from "express";
 import * as bodyParser from "body-parser";
 import { doctorRouter } from "./routes/doctorRouter";
+import { pacienteRouter } from "./routes/pacienteRouter";
+import { index } from "./routes/index";
 import { db } from "./db";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use("/doctores", doctorRouter);
 
+// motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", index);
+app.use("/doctores", doctorRouter);
+app.use("/pacientes", pacienteRouter);
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    titulo: "404",
+    descripcion: "Page Not Found...",
+  });
+});
 db.connect((err) => {
   if (err) {
     console.log("Database connection error");
