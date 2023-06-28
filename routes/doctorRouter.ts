10bdcdba09a084f8b3008a8ca49c2f5d8c496a6c
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-27 21:43:43 -0500

    feat(doctor):Ajuste de las rutas de  doctor

--- routes/doctorRouter.ts
+++ routes/doctorRouter.ts
@@ -1,43 +1,68 @@
 import express, { Request, Response } from "express";
 import * as doctorModel from "../models/doctor";
+import * as consultorioModel from "../models/consultorio";
+import { BasicConsultorio, Consultorio } from "../types/consultorio";
 import { BasicDoctor, Doctor, DoctorWithDetails } from "../types/doctor";
 const doctorRouter = express.Router();
 
 doctorRouter.get("/", async (req: Request, res: Response) => {
-  doctorModel.findAll((err: Error, doctores: Doctor[]) => {
-    if (err) {
-      return res.status(500).json({ errorMessage: err.message });
-    }
-    res.status(200).json({ data: doctores });
-  });
+  try {
+    doctorModel.findAll((err: Error, doctores: Doctor[]) => {
+      if (err) {
+        return res.status(500).json({ errorMessage: err.message });
+      }
+      res.render("doctores", { doctores: doctores, error: false, doctor: [] });
+      //res.status(200).json({ data: doctores });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 doctorRouter.post("/", async (req: Request, res: Response) => {
-  const newDoctor: Doctor = req.body;
-  doctorModel.create(newDoctor, (err: Error, doctorId: number) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
+  try {
+    const newDoctor: Doctor = req.body;
+    doctorModel.create(newDoctor, (err: Error, doctorId: number) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
 
-    res.status(200).json({ orderId: doctorId });
-  });
+      res.status(200).json({ orderId: doctorId });
+    });
+  } catch (error) {}
 });
 doctorRouter.get("/:id", async (req: Request, res: Response) => {
-  const doctorId = Number(req.params.id);
-  doctorModel.findOne(doctorId, (err: Error, order: Doctor) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    res.status(200).json({ data: order });
-  });
+  try {
+    const doctorId = Number(req.params.id);
+    doctorModel.findOne(doctorId, (err: Error, doctor: Doctor) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+      res.render("detaDoc", {
+        doctor: doctor,
+        error: false,
+      });
+      //res.status(200).json({ data: doctor });
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("detaDoc", {
+      error: true,
+      mensaje: "No se encuentra el documento...",
+    });
+  }
 });
 doctorRouter.put("/:id", async (req: Request, res: Response) => {
-  const doctor: Doctor = req.body;
-  doctorModel.update(doctor, (err: Error, numUpdate: number) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
+  try {
+    const doctor: Doctor = req.body;
+    doctorModel.update(doctor, (err: Error, numUpdate: number) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
 
-    res.status(200).json({ "Record(s) updated": numUpdate });
-  });
+      res.status(200).json({ "Record(s) updated": numUpdate });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 export { doctorRouter };
