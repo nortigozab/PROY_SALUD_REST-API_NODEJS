78a7073fe4b08263e59dbecf8a8e672192fc12c7
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-30 11:28:48 -0500

    feat(citaMedica): Agrege la solicitud para todos las citas medicas con una especialidad

--- models/citaMedica.ts
+++ models/citaMedica.ts
@@ -77,16 +77,17 @@ LEFT JOIN Pacientes p ON cm.IdPaciente = p.IdPaciente WHERE cm.IdCita=?`;
     callback(null, cita);
   });
 };
-export const findAll = (callback: Function) => {
+
+export const findAllEspe = (especialidadId: number, callback: Function) => {
   const queryString = `
   SELECT cm.IdCita, d.IdDoctor, d.Nombre as NombreDoc, d.Apellido as ApellidoDoc, e.IdEspecialidad, e.NombreEspecialidad, c.IdConsultorio, c.Piso, c.NumeroConsultorio, c.Disponibilidad, d.CorreoContacto, p.IdPaciente, p.Nombre as NombrePac, p.Apellido as ApellidoPac, p.NumeroCedula, p.FechaNacimiento, p.Telefono, e.IdEspecialidad, e.NombreEspecialidad, cm.Fecha, cm.Disponibilidad
 FROM CitasMedicas cm
 JOIN Doctores d ON cm.IdDoctor = d.IdDoctor
 JOIN Especialidades e ON e.IdEspecialidad = cm.Especialidad AND e.IdEspecialidad = d.Especialidad
 JOIN Consultorios c ON c.IdConsultorio = d.Consultorio
-LEFT JOIN Pacientes p ON cm.IdPaciente = p.IdPaciente`;
+LEFT JOIN Pacientes p ON cm.IdPaciente = p.IdPaciente WHERE e.IdEspecialidad=?`;
 
-  db.query(queryString, (err, result) => {
+  db.query(queryString, especialidadId, (err, result) => {
     if (err) {
       callback(err);
     }
@@ -121,6 +122,62 @@ LEFT JOIN Pacientes p ON cm.IdPaciente = p.IdPaciente`;
           pacienteId: row.IdPaciente,
           nombre: row.Nombre,
           apellido: row.Apellido,
+          numeroCedula: row.NumeroCedula,
+          fechaNacimiento: row.FechaNacimiento,
+          telefono: row.Telefono,
+        },
+        fecha: row.Fecha,
+        disponibilidad: row.Disponibilidad,
+      };
+      citas.push(cita);
+    });
+    callback(null, citas);
+  });
+};
+export const findAll = (callback: Function) => {
+  const queryString = `
+  SELECT cm.IdCita, d.IdDoctor, d.Nombre as NombreDoc, d.Apellido as ApellidoDoc, e.IdEspecialidad, e.NombreEspecialidad, c.IdConsultorio, c.Piso, c.NumeroConsultorio, c.Disponibilidad, d.CorreoContacto, p.IdPaciente, p.Nombre as NombrePac, p.Apellido as ApellidoPac, p.NumeroCedula, p.FechaNacimiento, p.Telefono, e.IdEspecialidad, e.NombreEspecialidad, cm.Fecha, cm.Disponibilidad
+FROM CitasMedicas cm
+JOIN Doctores d ON cm.IdDoctor = d.IdDoctor
+JOIN Especialidades e ON e.IdEspecialidad = cm.Especialidad AND e.IdEspecialidad = d.Especialidad
+JOIN Consultorios c ON c.IdConsultorio = d.Consultorio
+LEFT JOIN Pacientes p ON cm.IdPaciente = p.IdPaciente order by cm.IdCita asc`;
+
+  db.query(queryString, (err, result) => {
+    if (err) {
+      callback(err);
+    }
+
+    const rows = <RowDataPacket[]>result;
+    const citas: CitaMedica[] = [];
+
+    rows.forEach((row) => {
+      const cita: CitaMedicaWithDetails = {
+        citaId: row.IdCita,
+        especialidad: {
+          especialidadId: row.IdEspecialidad,
+          nombreEspecialidad: row.NombreEspecialidad,
+        },
+        doctor: {
+          doctorId: row.IdDoctor,
+          nombre: row.NombreDoc,
+          apellido: row.ApellidoDoc,
+          especialidad: {
+            especialidadId: row.IdEspecialidad,
+            nombreEspecialidad: row.NombreEspecialidad,
+          },
+          consultorio: {
+            consultorioId: row.IdConsultorio,
+            divisionPiso: row.Piso,
+            numeroConsultorio: row.NumeroConsultorio,
+            disponibilidad: row.Disponibilidad,
+          },
+          correoContacto: row.CorreoContacto,
+        },
+        paciente: {
+          pacienteId: row.IdPaciente,
+          nombre: row.NombrePac,
+          apellido: row.ApellidoPac,
           numeroCedula: row.NumeroCedula,
           fechaNacimiento: row.FechaNacimiento,
           telefono: row.Telefono,
